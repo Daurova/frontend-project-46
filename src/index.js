da03c468a33f4f94086b9024b587bdd045a51a4d
Daurova <147259989+Daurova@users.noreply.github.com>
2026-02-03 09:44:37 +0300

    feat/step 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import parseFile from "./parsers.js";
+
 const genDiff = (filepath1, filepath2, format) => {
   // Здесь будет логика сравнения файлов
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
 
-
-  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
+  console.log('Данные из файлов:');
+  console.log('file1:', data1);
+  console.log('file2:', data2);
+  console.log('формат:', format);
+  
+  // Вернем пока просто сообщение
+  return 'Разница будет здесь';
 };
 
 export default genDiff;
