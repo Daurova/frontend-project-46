96c854da2b01cb43cc6f26b7a5b887921c54fd07
Daurova <147259989+Daurova@users.noreply.github.com>
2026-02-20 11:35:41 +0300

    step 4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 import parseFile from "./parsers.js";
+import _ from 'lodash'
+
+const stringifyValue = (value)=>{
+  if (typeof value ==='string') {
+    return value
+  } else {
+    return String(value)
+  }
+}
+
+const stylishDiff = (obj1, obj2) => {
+  const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
+  
+    const lines = allKeys.flatMap((key) => {
+    const has1 = _.has(obj1, key);
+    const has2 = _.has(obj2, key);
+
+    // Ключ только во втором файле
+    if (!has1 && has2) {
+      return `  + ${key}: ${stringifyValue(obj2[key])}`;
+    }
+    // Ключ только в первом файле
+    if (has1 && !has2) {
+      return `  - ${key}: ${stringifyValue(obj1[key])}`;
+    }
+    // Ключ есть в обоих
+    if (_.isEqual(obj1[key], obj2[key])) {
+      return `    ${key}: ${stringifyValue(obj1[key])}`;
+    }
+    // Значения отличаются — возвращаем массив из двух строк
+    return [
+      `  - ${key}: ${stringifyValue(obj1[key])}`,
+      `  + ${key}: ${stringifyValue(obj2[key])}`,
+    ];
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
+
+}
 
 const genDiff = (filepath1, filepath2, format) => {
   // Здесь будет логика сравнения файлов
@@ -11,7 +50,7 @@ const genDiff = (filepath1, filepath2, format) => {
   console.log('формат:', format);
   
   // Вернем пока просто сообщение
-  return 'Разница будет здесь';
+  return stylishDiff(data1, data2);
 };
 
 export default genDiff;
