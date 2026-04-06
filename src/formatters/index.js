de4604a36bdebf77347b13dc5a1aafdb62b0774c
Daurova <147259989+Daurova@users.noreply.github.com>
2026-04-06 11:14:49 +0300

    fix/removed json files from src and fixed formattes/index

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,17 @@ import stylish from './stylish.js'
 import plain from './plain.js'
 import json from './json.js'
 
-const formatters = {
-  stylish,
-  plain,
-  json,
-}
-
 const getFormat = (format) => {
-  return formatters[format]
+  switch (format) {
+    case 'stylish':
+      return stylish
+    case 'plain':
+      return plain
+    case 'json':
+      return json
+    default:
+      throw new Error(`Unknown format: ${format}`)
+  }
 }
 
 export default getFormat
--- src/index.js
+++ src/index.js
@@ -4,44 +4,6 @@ import parseFile from './parsers.js'
 import buildDiff from './diffBuilder/diffBuilder.js'
 import getFormat from './formatters/index.js'
 
-// const stringifyValue = (value) => {
-//   if (typeof value === 'string') {
-//     return value
-//   }
-//   else {
-//     return String(value)
-//   }
-// }
-
-// const stylishDiff = (obj1, obj2) => {
-//   const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)))
-
-//   const lines = allKeys.flatMap((key) => {
-//     const has1 = _.has(obj1, key)
-//     const has2 = _.has(obj2, key)
-
-//     // Ключ только во втором файле
-//     if (!has1 && has2) {
-//       return `  + ${key}: ${stringifyValue(obj2[key])}`
-//     }
-//     // Ключ только в первом файле
-//     if (has1 && !has2) {
-//       return `  - ${key}: ${stringifyValue(obj1[key])}`
-//     }
-//     // Ключ есть в обоих
-//     if (_.isEqual(obj1[key], obj2[key])) {
-//       return `    ${key}: ${stringifyValue(obj1[key])}`
-//     }
-//     // Значения отличаются — возвращаем массив из двух строк
-//     return [
-//       `  - ${key}: ${stringifyValue(obj1[key])}`,
-//       `  + ${key}: ${stringifyValue(obj2[key])}`,
-//     ]
-//   })
-
-//   return `{\n${lines.join('\n')}\n}`
-// }
-
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parseFile(filepath1)
   const data2 = parseFile(filepath2)
